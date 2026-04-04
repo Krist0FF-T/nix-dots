35088b75dd93036ba2604c8745a69c670fb73ed3
Krist0FF-T <155083075+Krist0FF-T@users.noreply.github.com>
2026-04-05 00:15:30 +0200

    add games.nix

---
+++ home-manager/games.nix
@@ -0,0 +1,25 @@
+{ pkgs }:
+{
+  home.packages = with pkgs; [
+    # a Minecraft launcher (MultiMC fork)
+    prismlauncher
+
+    # (formerly minetest)
+    # (try the game Glitch!)
+    luanti
+
+    # # really cool but too addictive
+    # factorio-demo
+    # mindustry
+
+    # veloren
+    supertuxkart
+    superTux
+    the-powder-toy # (little 2d sandbox)
+
+    # 0ad
+    # hedgewars
+    warzone2100
+    # freeciv
+  ];
+}
